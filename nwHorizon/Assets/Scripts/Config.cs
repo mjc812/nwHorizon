7c75f4e5654d727742a11e3b8cbf7ac52a294cb7
mjc812 <68936604+mjc812@users.noreply.github.com>
2024-01-20 12:45:29 -0800

    add cesium 3D tileset to scene

---
+++ nwHorizon/Assets/Scripts/Config.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using CesiumForUnity;
+using System;
+
+public class SetLocation : MonoBehaviour
+{
+    public Cesium3DTileset cesium3DTileset;
+
+    private void Awake() {
+        string google_cloud_api = Keys.google_cloud_api;
+        cesium3DTileset.url = "https://tile.googleapis.com/v1/3dtiles/root.json?key=" + google_cloud_api;
+    }
+}
+
