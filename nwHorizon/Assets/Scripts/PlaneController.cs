5eadcaa09933ff49e20762214b33381e646f703a
mjc812 <68936604+mjc812@users.noreply.github.com>
2024-01-20 16:45:21 -0800

    add pitch and yaw sensitivity values

--- nwHorizon/Assets/Scripts/PlaneController.cs
+++ nwHorizon/Assets/Scripts/PlaneController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour
 {
     public float lerpTime = 10.0f;
+    public float yawSensitivity = 5.0f;
+    public float pitchSensitivity = 5.0f;
 
     void Start()
     {
@@ -16,8 +18,8 @@ public class PlaneController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        mouseX *= 100.0f;
-        mouseY *= 100.0f;
+        mouseX *= 100.0f * yawSensitivity;
+        mouseY *= 100.0f * pitchSensitivity;
 
         Quaternion xRotation = Quaternion.AngleAxis(-1 * mouseY, Vector3.right);
         Quaternion yRotation = Quaternion.AngleAxis(-1 * mouseX, Vector3.forward);
