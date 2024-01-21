70054e9f2362031c643e92e737dea1ebb45d695c
mjc812 <68936604+mjc812@users.noreply.github.com>
2024-01-20 19:12:32 -0800

    add location text rotation

--- nwHorizon/Assets/Scripts/LocationTextHandler.cs
+++ nwHorizon/Assets/Scripts/LocationTextHandler.cs
@@ -5,13 +5,37 @@ using TMPro;
 
 public class LocationTextHandler : MonoBehaviour
 {
+    public Transform player;
+    public float rotationSpeed = 5.0f;
     public GameObject locationText;
     public TMP_Text countryText;
     public TMP_Text cityText;
+    private bool shouldFaceAway = false;
+
+    // void Update()
+    // {
+    //     if (shouldFaceAway)
+    //     {
+    //         Vector3 directionToPlayer = player.position - transform.position;
+    //         directionToPlayer.y = 0;
+
+    //         Quaternion targetRotation = Quaternion.LookRotation(-directionToPlayer);
+
+    //         transform.rotation = Quaternion.Slerp(
+    //             transform.rotation, targetRotation, rotationSpeed * Time.deltaTime
+    //         );
+    //     }
+    // }
 
     public void SetLocationText(string city, string country) {
         countryText.text = country;
         cityText.text = city;
+        //shouldFaceAway = false;
+        //Invoke("StartFacingAway", 10f);
+    }
+
+    private void StartFacingAway() {
+        shouldFaceAway = true;
     }
 
     public void SetLocationTextAltitude(float altitude) {
